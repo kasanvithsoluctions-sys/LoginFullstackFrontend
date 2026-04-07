bf3ef76bb9c40344f5955f65b58c93e7a823ad16
kasanvith <kasanvithsoluctions@gmail.com>
2026-04-07 04:29:55 -0300

    background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
 
 
-  return (
-    <>
-    </>
+
+
+
+
+
+function App(){
+
+return (
+  
+ <>
+ <main className="min-h-screen bg-gray-100">
+
+  <div className='flex flex-col justify-center'>
+
+
+
+  </div>
+
+
+ </main>
+
+ 
+   
+  
+
+        
+
+ 
+
+ 
+
+ 
+
+ </>
+ 
+
+  
+  
   )
 }
-
 export default App
+
+
